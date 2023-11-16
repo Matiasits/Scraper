646cc16f98a786c3b130a9077cb568adf6700edb
Matias Roldan <matias.roldan@itscordoba.edu.ar>
2023-11-16 19:56:16 -0300

    Condigo con response de titulos y enlaces de menus y submenus

--- scraper.py
+++ scraper.py
@@ -1,64 +1,51 @@
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+import requests
+from bs4 import BeautifulSoup
+import re
 
-# URL de la página web que deseas analizar
-url = 'https://www.paulinacocina.net/'
+li = [
+    'menu-item menu-item-type-taxonomy menu-item-object-category menu-item-has-children menu-item-11384',
+    'menu-item menu-item-type-taxonomy menu-item-object-category menu-item-has-children menu-item-451',
+    'menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-21292'
+    ]
+    
 
-# Realizar una solicitud HTTP para obtener el contenido de la página
-response = requests.get(url)
-
-# Verificar si la solicitud fue exitosa
-if response.status_code == 200:
-    # Parsear el contenido de la página web con BeautifulSoup
-    soup = BeautifulSoup(response.text, 'html.parser')
+def get_categorias_con_enlaces():
+    """Obtiene una lista de las categorías del menú de Paulina Cocina haciendo scraping, incluyendo los enlaces de cada subcategoría."""
     
-    # Inicializar listas para los títulos y enlaces
-    titulos_h1 = []
-    titulos_h2 = []
-    titulos_h3 = []
-    enlaces = []
-    
-    # Encontrar todos los elementos de título (h1, h2, h3) en la página
-    for nivel in range(1, 4):
-        etiqueta = f'h{nivel}'
-        titulos = soup.find_all(etiqueta)
-        for titulo in titulos:
-            texto = titulo.text.strip()
-            if nivel == 1:
-                titulos_h1.append(texto)
-            elif nivel == 2:
-                titulos_h2.append(texto)
-            elif nivel == 3:
-                titulos_h3.append(texto)
-    
-    # Encontrar todos los enlaces en la página
-    enlaces = soup.find_all('a')
-    
-    # Mostrar los títulos h1, h2 y h3
-    print("Títulos h1:")
-    for titulo in titulos_h1:
-        print(titulo)
-    
-    print("\nTítulos h2:")
-    for titulo in titulos_h2:
-        print(titulo)
-    
-    print("\nTítulos h3:")
-    for titulo in titulos_h3:
-        print(titulo)
-    
-    # Mostrar las URLs de los enlaces
-    print("\nEnlaces:")
-    for enlace in enlaces:
-        # Obtener el atributo 'href' del enlace
-        href = enlace.get('href')
+    for i in li:    
+        # Crear una solicitud HTTP a la página web.
+        response = requests.get("https://www.paulinacocina.net/")
+        # Obtener el código HTML de la respuesta.
+        html = response.content
+        # Usar BeautifulSoup para parsear el código HTML.
+        soup = BeautifulSoup(html, "html.parser")
+        # Buscar las etiquetas `<li>` con la clase `categoria`.
+        categorias = soup.find_all("li", class_=i)
+        # Extraer los nombres de las categorías.
+        nombres = [categoria.find("a").text for categoria in categorias]
+        # Extraer los enlaces de las categorías.
+        enlaces = [categoria.find("a")["href"] for categoria in categorias]
+        # Devolver una lista con los nombres y enlaces de las categorías.
+        print(list(zip(nombres, enlaces)))
         
-        # Combinar la URL base con la URL relativa del enlace, si es necesario
-        enlace_completo = urljoin(url, href)
         
-        # Imprimir la URL completa
-        print(enlace_completo)
+        for j in enlaces:
+            # Crear una solicitud HTTP a la página web.
+            response = requests.get(j)
+            # Obtener el código HTML de la respuesta.
+            html = response.content
+            # Usar BeautifulSoup para parsear el código HTML.
+            soup = BeautifulSoup(html, "html.parser")
+            # Buscar las etiquetas `<li>` con la clase `categoria`.
+            categoriasSub = soup.find_all("li", class_=re.compile("menu-item"))
+            # Iterar sobre las subcategorías y extraer nombres y enlaces.
+            for categoria_sub in categoriasSub:
+                nombre_sub = categoria_sub.find("a").text
+                enlace_sub = categoria_sub.find("a")["href"]
+                print(f"Subcategoría: {nombre_sub}, Enlace: {enlace_sub}")
+    
         
-else:
-    print('No se pudo acceder a la página:', response.status_code)
+get_categorias_con_enlaces()
